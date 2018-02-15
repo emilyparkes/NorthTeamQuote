92dec84539af14f705a82309b9deac2a208d55a2
catherinefromont <catherinegfromont@gmail.com>
2018-02-16 11:15:35 +1300

    Data file is working.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 import ErrorMessage from './ErrorMessage'
 import WaitIndicator from './WaitIndicator'
 import Home from './Home'
+import data from '../../data.js'
 
-const App = () => (
-  <div className='app'>
-    <ErrorMessage />
-    <WaitIndicator />
-    <Home />
-  </div>
-)
+const App = () => {
+  console.log(data)
+  return (
+    <div className='app'>
+      <ErrorMessage />
+      <WaitIndicator />
+      <Home />
+      <img alt='adam' src={data.person[0].image}/>
+    </div>
+  )
+}
 
 export default App
--- data.js
+++ data.js
@@ -1,9 +1,13 @@
+const adamImg = '/profiles/png/adam.png'
 const data = {
   person: [
     {
-      id:
-      name: 
-      
+      id: 1,
+      name: 'Adam',
+      quote: 'Its not the load that breaks you down, its the way you carry it.',
+      image: adamImg
     }
   ]
 }
+
+export default data
